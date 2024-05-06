47a4e5706fbdf90885e82a98acb6e4d6ac1ec0ed
armando <rsb987@hotmail.com>
2024-05-05 18:20:28 -0600

    default position

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import Position from "./js/position.js"
         //position.setupFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", chessboard);
         //position.setupFromFen("8/8/8/1Q6/8/8/k1K5/8 w - - 0 1", chessboard);
         //position.setupFromFen("5NR1/5BKP/8/4b1k1/8/8/8/8 b - - 0 1", chessboard);
-        position.setupFromFen("8/7b/1k6/1q6/8/8/KPP5/2B5 b - - 0 1", chessboard);
+        //position.setupFromFen("8/7b/1k6/1q6/8/8/KPP5/2B5 b - - 0 1", chessboard);
         //chessboard.setBlackTurn();
 
         ui.draw();
