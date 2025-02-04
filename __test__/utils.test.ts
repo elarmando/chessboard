a2e47494def6d390f33ece95adbc68a5e7b8085c
Armando Serrato <rsb987@hotmail.com>
2025-02-04 11:42:18 -0600

    first unit in typescript

--- __test__/utils.test.ts
+++ __test__/utils.test.ts
@@ -3,7 +3,7 @@ import Utils from '../src/core/utils';
 import ChessBoard from '../src/core/board';
 import Position from '../src/core/position';
 
-test("Utils_getMoveAsString_pawnMove", ()=>{
+test("Utils_getMoveAsString_pawnMove", () => {
     var chessBoard = new ChessBoard();
     var position = new Position();
 
@@ -15,4 +15,18 @@ test("Utils_getMoveAsString_pawnMove", ()=>{
     var str = utils.getMoveAsString(squarea2, squarea4);
 
     expect(str).toBe("a4");
-})
+});
+
+test("Utils_getMoveAsString_pawnTakes", () => {
+    var chessBoard = new ChessBoard();
+    var position = new Position();
+
+    position.setupDefault(chessBoard);
+    chessBoard.move("e2", "e4");
+    chessBoard.move("d7", "d5");
+
+    //var square1 = 
+    //var utils = new Utils(chessBoard);
+
+});
+
